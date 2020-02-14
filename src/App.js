8edd705b59c9222c5ca5e338d0e4c6f8a486037d
Cory Booth <Rodimus2005@hotmail.com>
2020-02-14 12:10:53 -0600

    search function and api test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ApiDemo extends Component {
+  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+        tempF: [],
+        search: '',
+    };
 }
 
-export default App;
+  handleSearch = () => event => {
+    this.setState({
+        ...this.state.search,
+        search: event.target.value,
+    });
+  }
+
+
+  apiCall = () => {
+    
+    let apiKey = '696e9369164547f080e155915201402';
+
+    axios({
+        url: `http://api.weatherapi.com/v1/current.json?key=696e9369164547f080e155915201402&q=${this.state.search}`,
+        method: 'GET',
+    })
+        .then(response => {
+            let weather = response.data.current.temp_f;
+            console.log(response.data);
+            console.log(weather);
+            this.setState({
+                tempF: weather,
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+  };
+
+  render() {
+      // const { classes } = this.props;
+      return (
+          <div>
+              <button onClick={this.apiCall}>Go for temp! </button>
+              The temperature in <input className="input" onChange={this.handleSearch()} value={this.state.search} placeholder='location' /> is {this.state.tempF} degrees F right now!
+
+              
+
+          </div>
+      );
+
+  }
+}
+
+export default ApiDemo;
