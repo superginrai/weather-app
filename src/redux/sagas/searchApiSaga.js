cbf52fd3bdc41c0ef9b9606ee0d52afefef63426
Cory Booth <Rodimus2005@hotmail.com>
2020-02-19 10:31:40 -0600

    api endpoint update

--- src/redux/sagas/searchApiSaga.js
+++ src/redux/sagas/searchApiSaga.js
@@ -4,7 +4,7 @@ import { takeEvery, call, put as dispatch } from 'redux-saga/effects';
 function* searchApi(action) {
     try {
         console.log('are we there yet?', action.payload)
-        const search = yield call (axios.get, `http://api.weatherapi.com/v1/forecast.json?key=696e9369164547f080e155915201402&q=${action.payload}&days=4`);
+        const search = yield call (axios.get, `https://api.weatherapi.com/v1/forecast.json?key=696e9369164547f080e155915201402&q=${action.payload}&days=4`);
         console.log(search);
         yield JSON.stringify(search);
         yield dispatch({
