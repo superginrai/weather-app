47e435f2723ab98a2f45bfade3ec8fa7401018dc
Cory Booth <Rodimus2005@hotmail.com>
2020-02-20 14:40:43 -0600

    clean up

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
 import React, { Component } from 'react';
 import './index.css';
 import ForecastPage from './components/ForecastPage/ForecastPage';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <Router>
-//           <Switch>
-//             <Redirect exact from="/" to="/forecast" />
-//             <Route
-//               path="/search"
-//               component={SearchPage} />
-//             <Route
-//               path="/forecast"
-//               component={ForecastPage} />
-//           </Switch>
-//         </Router>
-
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 import './index.css';
 import App from './App';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { takeEvery, call, put as dispatch } from 'redux-saga/effects';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
 
@@ -15,70 +14,10 @@ import reducer from './redux/reducers';
 
 import rootSaga from './redux/sagas';
 
-// const weather = (state = [], action) => {
-//     if (action.type === 'SEARCH_WEATHER_API') {
-//         axios({
-//             url: `http://api.weatherapi.com/v1/current.json?key=696e9369164547f080e155915201402&q=${action.payload}`,
-//             method: 'GET',
-//         })
-//             .then(response => {
-                 
-//             })
-//             .catch(err => {
-//                 console.error(err);
-//             });
-//         return [...state, ...action.payload];
-
-//     } else if (action.type === 'RESET') {
-//         return [];
-//     }
-//     return state;
-// };
-
-// const searchResults = (state = [], action) => {
-//     switch (action.type) {
-//         case 'SEARCH_RESULTS':
-//             return action.payload;
-//         default:
-//             return state;
-//     }
-// };
-
-// const searchResults = (state = [], action) => {
-//     if (action.type === 'SEARCH_RESULTS') {
-//         console.log(action);
-//         return [...state, ...action.payload];
-//     } else if (action.type === 'RESET') {
-//         return [];
-//     }
-//     return state;
-// };
-
-// const tacos = (state = [], action) => {
-//     if (action.type === 'SEARCH_WEATHER_API') {
-//         console.log(action);
-//         return [...state, ...action.payload];
-//     } else if (action.type === 'RESET') {
-//         return [];
-//     }
-// };
-
-// const storeInstance = createStore(
-//     combineReducers({
-//         searchResults,
-//         // tacos,
-//     }),
-//    // applyMiddleware(logger),
-// )
-
 const preloadedState = {};
 const middlewares = [];
 const sagaMiddleware = createSagaMiddleware();
 middlewares.push(sagaMiddleware);
-
-// if (process.env.NODE_ENV === 'development') {
-//   middlewares.push(logger);
-// }
 
 const storeInstance = createStore(
   reducer,
