6dea7efd02aa04e7ff26ce5a1148958ed396972f
Cory Booth <Rodimus2005@hotmail.com>
2020-03-10 11:38:23 -0500

    env file

--- src/redux/sagas/searchApiSaga.js
+++ src/redux/sagas/searchApiSaga.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { takeEvery, call, put as dispatch } from 'redux-saga/effects';
 import swal from 'sweetalert';
 
-const apiKey = '696e9369164547f080e155915201402'
+//const apiKey = '696e9369164547f080e155915201402'
 
 //Sends call to WeatherAPI.com and dispatches results to searchResultsReducer for storage
 function* searchApi(action) {
@@ -10,7 +10,7 @@ function* searchApi(action) {
         yield dispatch({
             type: 'CLEAR_RESULTS',
         })
-        const search = yield call (axios.get, `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${action.payload}&days=4`);
+        const search = yield call (axios.get, `https://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_API_KEY}&q=${action.payload}&days=4`);
         yield JSON.stringify(search);
         yield dispatch({
             type: 'SEARCH_RESULTS',
